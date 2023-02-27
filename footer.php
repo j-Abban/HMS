6d76934a03415c753ea3bbf07e521934b46e3e73
Justice Abban <91249181+Just-tice2002@users.noreply.github.com>
2023-02-27 23:44:57 +0000

    I needed to make these changes.

---
+++ footer.php
@@ -0,0 +1,57 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p> A Hospital Management System (HMS) refers to a system designed to manage healthcare data. This includes systems that collect, store, manage and transmit a patient’s electronic medical record (EMR), a hospital’s operational management or a system supporting healthcare policy decisions. </p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>SOCIAL LINKS</h6>
+            <p><a href="https://www.itsourcecode.com/">Official Website</a></p>
+            <p><a href="https://www.facebook.com/itsourcecode">Facebook</a></p>
+            <p><a href="https://www.youtube.com/c/JokenVillanueva?sub_confirmation=1">Youtube</a></p>
+            <p><a href="https://www.twitter.com/itsourcecode">Twitter</a></p>
+            <p><a href="https://www.linkedin.com/in/joken-villanueva-776b99157/">LinkedIn</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© 2022 Hospital Management System | Developed By: <strong><span></span><a href="https://www.itsourcecode.com/" style="color: white">ITSOURCECODE</a></span></strong>  </p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
