a6b4d5fdf8641430a921c759a5cc534949997bfa
j-Abban <justice.abban@amalitech.org>
2023-06-22 18:38:47 +0000

    first commit

---
+++ about.php
@@ -0,0 +1,51 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Up At Hospital Management System</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <ul class="row">
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                <p>HMS provide reliable communications and improve public safety areas. </p>
+                              </li>
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                <p>Hopital Management System has licensed and qualified doctors! </p>
+                              </li>
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                <p>Hospital Management System Supports Online Appointment. Check the appointment tab above. </p>
+                              </li>
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                <p>HMS qualified doctors offers free counseling to their patients. </p>
+                              </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<?php include 'footer.php';?>
